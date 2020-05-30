9d1ecabc7759a9e13372cd90e9c036000d0d6c46
mita <iaoiui0727@gmail.com>
2020-05-31 00:51:17 +0900

    modify scenario-server for api-server

--- api-server/srcs/controllers/related_terms_controller.py
+++ api-server/srcs/controllers/related_terms_controller.py
@@ -19,21 +19,22 @@ def get_synonym():
 
         # GiNZA（SudachiPy）による名詞抽出
         noun_list = GinzaWrapper().get_noun(sentence)
-
-        # fastTextで意味の近い単語を抽出
-        ft = FasttextWrapper()
-        for noun in noun_list:
-            similar_words = ft.get_similar_words(noun.lower())
-            record = {"keyword": noun, "similarWords": []}
-            for similar_word in similar_words:
-                record["similarWords"].append(
-                    {"word": similar_word[0], "value": similar_word[1]}
-                )
-            response["words"].append(record)
-
+        get_synonym_core(noun_list, response)
         logger.info("finish synonym")
         return jsonify(response)
     except Exception as error:
         logger.error(error)
         logger.info("finish synonym")
         return jsonify({"error": "error"})
+
+# TODO 高速化
+def get_synonym_core(noun_list, response):
+  ft = FasttextWrapper()
+  for noun in noun_list:
+      similar_words = ft.get_similar_words(noun.lower())
+      record = {"keyword": noun, "similarWords": []}
+      for similar_word in similar_words:
+          record["similarWords"].append(
+              {"word": similar_word[0], "value": similar_word[1]}
+          )
+      response["words"].append(record)
